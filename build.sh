e5333cc22a224a3bdebec7f2fde7b417c2e88b32
Your Name <you@example.com>
2019-10-06 15:01:29 +0000

    wip

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 tag=$(git rev-parse HEAD)
 image=$(grep AWS_ACCOUNT_NUMBER docker/web/development.env | cut -d= -f2-).dkr.ecr.us-east-1.amazonaws.com/chickpea-app
+
+#save variables
 echo $tag > tag.txt
 echo $image > image.txt
 
--- test.sh
+++ test.sh
@@ -2,6 +2,10 @@
 #run tests
 docker-compose run web bash -c "rake db:create && rake db:migrate && rake"
 
+#get variables
+tag=$(cat tag.txt)
+image=$(cat image.txt)
+
 #upload to ECR
 docker push $image:$tag
 docker push $image
