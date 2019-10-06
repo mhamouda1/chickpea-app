09fafeb731ade8f18760b292d95a69dae4c4d247
Your Name <you@example.com>
2019-10-06 14:48:54 +0000

    wip

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 tag=$(git rev-parse HEAD)
-image=$(ruby -e "puts '`cat docker-compose.yml | grep amazon`'.split(' ').last.split(':').first")
+image=$(grep AWS_ACCOUNT_NUMBER docker/web/development.env | cut -d= -f2-).dkr.ecr.us-east-1.amazonaws.com/chickpea-app
 
 #build image
 docker build -t $image:$tag -t $image:latest -f docker/web/Dockerfile .
