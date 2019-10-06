5a1e84c0193f127a54132615d2e767d408116205
Your Name <you@example.com>
2019-10-06 18:43:50 +0000

    wip

--- jenkins/provision.sh
+++ jenkins/provision.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-echo 'deploying script'
+#rake db:create RAILS_ENV=production
+#rake db:migrate RAILS_ENV=production
+#rake assets:precompile RAILS_ENV=production
+docker-compose run -p 5000:3000 web bash -c "bundle install RAILS_ENV=production && rake db:create RAILS_ENV=production && rake db:migrate RAILS_ENV=production && rake assets:precompile RAILS_ENV=production"
 
-#set db password/username
+#upload assets to S3
+aws s3 cp public/assets/ s3://chickpea-app-cb3409c1ffb8f57ce08a9e/assets/ --recursive --include "*" --acl "public-read" && \
+
+#issue command to kubernetes to "ruby kubernetes/deploy.rb 2398392398293874409"
