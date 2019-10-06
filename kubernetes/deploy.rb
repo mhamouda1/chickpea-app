8cd5f229ccb7e0d34bb1617e1c6ea133f82c956f
Your Name <you@example.com>
2019-10-06 18:04:11 +0000

    wip

--- kubernetes/deploy.rb
+++ kubernetes/deploy.rb
@@ -1,7 +1,7 @@
 require 'pry'
 #`git clone https://github.com/mhamouda1/chickpea-app`
 
-x = `cat kubernetes/deploy.yml.template`.gsub("REPLACE_AWS_ACCOUNT_NUMBER", "123")
+x = `cat kubernetes/deploy.yml.template`.gsub("REPLACE_AWS_ACCOUNT_NUMBER", "316812563398")
 git_commit = `git rev-parse HEAD`.chomp
 x = x.gsub("REPLACE_GIT_SHA", git_commit)
 `echo "#{x.chomp}" > kubernetes/deploy.yml`
