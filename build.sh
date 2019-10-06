d4040cbe66aa6ec6db932ccd8d5353d5bfbc8f59
Your Name <you@example.com>
2019-10-06 12:54:20 +0000

    wip

--- build.sh
+++ build.sh
@@ -4,13 +4,12 @@ image=$(ruby -e "puts '`cat docker-compose.yml | grep amazon`'.split(' ').last.s
 
 
 #build image
-sudo docker build -t $image:$tag -t $image:latest -f docker/web/Dockerfile .
+docker build -t $image:$tag -t $image:latest -f docker/web/Dockerfile .
 
 #run tests
-sudo /usr/local/bin/docker-compose run web bash -c "rake db:create && rake db:migrate && rake"
+docker-compose run web bash -c "rake db:create && rake db:migrate && rake"
 
 #upload to ECR
 sudo $(aws ecr get-login --no-include-email --region us-east-1)
 sudo docker push $image:$tag
 sudo docker push $image
-
