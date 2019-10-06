49f4f423c0203d8232b7af8686ca26673bc403c3
Your Name <you@example.com>
2019-10-06 18:44:45 +0000

    wip

--- jenkins/provision.sh
+++ jenkins/provision.sh
@@ -3,7 +3,7 @@
 #rake db:create RAILS_ENV=production
 #rake db:migrate RAILS_ENV=production
 #rake assets:precompile RAILS_ENV=production
-docker-compose run -p 5000:3000 web bash -c "bundle install RAILS_ENV=production && rake db:create RAILS_ENV=production && rake db:migrate RAILS_ENV=production && rake assets:precompile RAILS_ENV=production"
+docker-compose run -p 5000:3000 web bash -c "bundle install && rake db:create RAILS_ENV=production && rake db:migrate RAILS_ENV=production && rake assets:precompile RAILS_ENV=production"
 
 #upload assets to S3
 aws s3 cp public/assets/ s3://chickpea-app-cb3409c1ffb8f57ce08a9e/assets/ --recursive --include "*" --acl "public-read" && \
