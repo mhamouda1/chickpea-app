ce91157396a801f430466e69f588267c7feeb488
Your Name <you@example.com>
2019-10-07 03:56:25 +0000

    wip

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -4,8 +4,8 @@ tag=$(cat tag.txt)
 image=$(cat image.txt)
 
 #run tests
-docker-compose run web bash -c "rake db:create && rake db:migrate"
-docker-compose run web bash -c "rake" #running tests needs to be independent of the migrations
+docker-compose run web bash -c "rake db:create && rake db:migrate && rake"
+# docker-compose run web bash -c "rake" #running tests needs to be independent of the migrations
 
 #upload to ECR
 # $(aws ecr get-login --no-include-email --region us-east-1)
