22ac4b2130e70172d7da432a92fec13a7ea625d8
Your Name <you@example.com>
2019-10-07 01:56:45 +0000

    wip

--- jenkins/inputs/database.sh
+++ jenkins/inputs/database.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ES_HOST=$(cat ES_HOST.txt)
-DATABASE_NAME=appmysql
+DATABASE_NAME=chickpea
 DATABASE_ENV=default
 
 results=$(curl -XGET "http://$ES_HOST:9200/chickpea/_search" -H 'Content-Type: application/json' -d'{  "query": {    "bool" : {      "must" : [        {  "term" : { "database.name.keyword": "'$DATABASE_NAME'" }},        {  "term" : { "database.environment.keyword" : "'$DATABASE_ENV'" }}      ]    }  }}')
