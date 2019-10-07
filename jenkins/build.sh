2a6e2e4758f51fc827674fb3d0af78200bf0df32
Your Name <you@example.com>
2019-10-07 03:42:41 +0000

    wip

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-INPUT_ARTIFACTS_DIR=$(echo cat INPUT_ARTIFACTS_DIR.txt)
-echo $INPUT_ARTIFACTS_DIR
+INPUT_ARTIFACTS_DIR=$(cat INPUT_ARTIFACTS_DIR.txt)
 tag=$(git rev-parse HEAD)
-image=$(grep AWS_ACCOUNT_NUMBER docker/web/development.env | cut -d= -f2-).dkr.ecr.us-east-1.amazonaws.com/chickpea-app
+image=$(cat $INPUT_ARTIFACTS_DIR/ecr/url.txt)
 
 #save variables
 echo $tag > tag.txt
