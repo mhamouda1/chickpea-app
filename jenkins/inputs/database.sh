3cc76f9a7e565866b634ec659094f3cc1d4e12bf
Your Name <you@example.com>
2019-10-07 01:55:28 +0000

    wip

--- jenkins/inputs/database.sh
+++ jenkins/inputs/database.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 ES_HOST=$(cat ES_HOST.txt)
+DATABASE_NAME=appmysql
+DATABASE_ENV=default
 
-results=$(curl -XGET "http://$ES_HOST:9200/chickpea/_search" -H 'Content-Type: application/json' -d'{  "query": {    "bool" : {      "must" : [        {  "term" : { "database.name.keyword": "chickpea" }},        {  "term" : { "database.environment.keyword" : "production" }}      ]    }  }}')
+results=$(curl -XGET "http://$ES_HOST:9200/chickpea/_search" -H 'Content-Type: application/json' -d'{  "query": {    "bool" : {      "must" : [        {  "term" : { "database.name.keyword": "'$DATABASE_NAME'" }},        {  "term" : { "database.environment.keyword" : "'$DATABASE_ENV'" }}      ]    }  }}')
 
-echo $results > blah.txt
+echo $results > results.txt
+# cat results.txt | jq .hits.hits[0]._source
+
+
